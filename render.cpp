633d6eda66399ff699b8d33e1e1fdba7ebc731c5
DanielQ-51 <danielhqin1127@gmail.com>
2026-07-08 22:20:53 -0400

    final edits

--- render.cpp
+++ render.cpp
@@ -25,7 +25,66 @@ void renderPixel(
 
     // ---------------------- Begin your code below --------------------------------
 
+     // A value that encodes the effects of bouncing from the current point, 
+    // all the way backwards to the camera. It is used by multiplying with the
+    // emission of lights, so it starts at 1
+    float3 throughput = f3(1.0f, 1.0f, 1.0f);
 
+    Ray r = camera.generateCameraRay(x, y);
+
+    for (int depth = 0; depth < BOUNCE_DEPTH; depth++) {
+        Intersection intersect = traceClosestHit(r, positions, normals, mesh);
+
+        if (!intersect.isValid) {
+            break;
+        }
+
+        float3 position;
+        float3 normal;
+        float3 emission;
+        float3 color;
+
+        fetchGeometry(
+            intersect,
+            positions,
+            normals,
+            mesh,
+
+            position,
+            normal, 
+            color,
+            emission
+        );
+
+        if (lengthSquared(emission) > 0.0f) {
+            colors_sum += emission * throughput;
+        }
+
+        float3 bsdf_value;
+        float3 newDirection;
+        float pdf;
+
+        sampleBSDF(
+            normal,
+            color,
+            r.direction,
+
+            newDirection,
+            bsdf_value,
+            pdf
+        );
+
+        if (pdf <= 1E-6) {
+            break;
+        }
+
+        float surface_cosine = std::abs(dot(newDirection, normal));
+
+        throughput *= bsdf_value * surface_cosine / pdf;
+
+        r.direction = newDirection;
+        r.origin = position + newDirection * 0.001f;
+    }
 
 
 
--- utils.h
+++ utils.h
@@ -274,8 +274,8 @@ inline void readObjSimple(
 }
 
 inline float randomFloat() {
-    static std::mt19937 generator(std::random_device{}());
-    static std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+    thread_local std::mt19937 generator(std::random_device{}());
+    thread_local std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
     
     return distribution(generator);
 }
